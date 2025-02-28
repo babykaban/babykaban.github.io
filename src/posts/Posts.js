f8abc65ec7345a1960a0bd1c33060af664700e03
BabyKaban <paul92219@proton.me>
2025-02-27 20:06:12 -0800

    Adding old blog posts and starting a new one

--- src/posts/Posts.js
+++ src/posts/Posts.js
@@ -1,4 +1,6 @@
-// import editor_update_triangle_subtraction from '../posts/2024-08-18-editor-update-triangle-subtraction.md';
+import clipper2_reimplementation from '../posts/2024-10-23-clipper2-reimplementation.md';
+import editor_update_triangulation from '../posts/2024-09-02-editor-update-triangulation.md';
+import editor_update_triangle_subtraction from '../posts/2024-08-18-editor-update-triangle-subtraction.md';
 import editor_update_map_mode from '../posts/2024-08-04-editor-update-map-mode.md';
 import editor_update_sswm_format from '../posts/2024-07-29-editor-update-sswm-format.md';
 
@@ -7,14 +9,35 @@ import editor_update_sswm_formatImg from '../assets/img/editor_picture_4.png';
 
 const Posts = [
     {
+        title: "Clipper2 Reimplementation",
+        date: "October 23, 2024",
+        description: "Clipper2: A new implementation of the Clipper library",
+        image: editor_update_sswm_formatImg,
+        content: clipper2_reimplementation,
+        categories: ["Clipper2"],
+        route: "/2024-10-23-clipper2-reimplementation"
+    },
+
+    {
+        title: "Triangulation",
+        date: "September 2, 2024",
+        description: "Editor update: Triangulation",
+        image: editor_update_sswm_formatImg,
+        content: editor_update_triangulation,
+        categories: ["Editor"],
+        route: "/2024-09-02-editor-update-triangulation"
+    },
+
+    {
         title: "Triangle Boolean Subtraction",
         date: "August 18, 2024",
         description: "Editor update: Triangle Boolean Subtraction",
         image: editor_update_sswm_formatImg,
-        content: editor_update_sswm_format,
+        content: editor_update_triangle_subtraction,
         categories: ["Editor"],
         route: "/2024-08-18-editor-update-triangle-subtraction"
     },
+
     {
         title: "Expanding the World Map Editor Mode",
         date: "August 4, 2024",
